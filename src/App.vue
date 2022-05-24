c6afd65e8ea1eb627a0a47393a4a208a367f55d4
Torhlex <94188402+Torhlex@users.noreply.github.com>
2022-05-24 14:07:18 +0100

    starting

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="Logo">
+        <img
+          class="logo"
+          src="./assets/BfreeLogo.png"
+          onClick="location.href='/LandingPage.vue'"
+          alt="BfreeLogo"
+          width="130"
+          height="50"
+        />
+      </div>
+      <div class="TopNaav">
+        <router-link to="/about">About</router-link>
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+import LandingPage from "./components/LandingPage.vue";
+
+export default {
+  name: "app",
+  Components: {
+    LandingPage,
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
+/* .logo {
+  display: flex;
+  justify-content: start;
+} */
+
+img {
+  cursor: pointer;
+}
+
 nav {
+  display: flex;
+  justify-content: space-between;
   padding: 30px;
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img
+      class="logo"
+      src="img/BfreeLogo.png"
+      onClick="location.href='index.html'"
+      alt="BfreeLogo"
+      width="160"
+      height="70"
+    /> -->
+    <LandingPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LandingPage from "@/components/LandingPage.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    LandingPage,
+  },
+};
 </script>
